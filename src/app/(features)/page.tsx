b4e52dc0f9825aee249811f6f8147395a592508a
Atharva Mahadeokar <athaarva999@gmail.com>
2023-10-13 12:33:23 +0530

    bug fixes 4

--- src/app/(features)/page.tsx
+++ src/app/(features)/page.tsx
@@ -1,8 +1,7 @@
 import Head from "next/head";
 import { HomeNavBar } from "@/app/_components/Header/navbar";
 import { HomeFooter } from "@/app/_components/Footer/footer";
-import { getPersonaRecommendationItems } from "../_helpers/getPersonaRecommendationItems";
-import { allPersonaRecommendationGenraList } from "../_constants/recommended_genra_list";
+
 
 import Hero from "../_components/Hero";
 import Products from "../_components/Trending_Product_listing/Products";
